f979ecc0fe958044437cf45ea0f61893ab34ed43
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-10 14:43:26 +0200

    condition for 0 as divider

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (dividend === "" || divider === ""){
+  if (divider === 0){
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+  } else if (dividend === "" || divider === ""){
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
   } else{
   const divisionResult = dividend / divider;
