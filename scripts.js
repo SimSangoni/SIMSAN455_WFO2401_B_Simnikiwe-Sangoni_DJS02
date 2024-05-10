9d77dc541e00877f8e57b42545e15e119d64da15
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-10 14:38:45 +0200

    Dividing numbers result in a decimal number

--- scripts.js
+++ scripts.js
@@ -5,5 +5,6 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const divisionResult = dividend / divider;
+  result.innerText = Math.floor(divisionResult)
 });
