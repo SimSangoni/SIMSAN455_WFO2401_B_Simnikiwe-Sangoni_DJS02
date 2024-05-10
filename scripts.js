2867ce970264a4ad78b6339bede952d0e2eb39b6
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-10 14:56:12 +0200

    conditino for non numeric number added

--- scripts.js
+++ scripts.js
@@ -6,13 +6,17 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (divider === "0"){
-    result.innerText = "Division not performed. Invalid number provided. Try again.";
-    console.error("Math Error! Division by zero attempted.");
-  } else if (dividend === "" || divider === ""){
-    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-  } else{
-  const divisionResult = dividend / divider;
-  result.innerText = Math.floor(divisionResult)
+  if (isNaN(dividend) || isNaN(divider)){
+    console.error("Non-numeric input detected.");
+  } else {
+    if (divider === "0"){
+      result.innerText = "Division not performed. Invalid number provided. Try again.";
+      console.error("Math Error! Division by zero attempted.");
+    } else if (dividend === "" || divider === ""){
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    } else{
+    const divisionResult = dividend / divider;
+    result.innerText = Math.floor(divisionResult)
+    }
   }
 });
